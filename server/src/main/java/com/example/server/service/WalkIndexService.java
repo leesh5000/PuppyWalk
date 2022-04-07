e05c0a070bed5587697f075a472023afbcde2ca7
leesh5000 <leesh50000@gmail.com>
2022-04-08 00:38:14 +0900

    READ TIME OUT BUG FIX

--- server/src/main/java/com/example/server/service/WalkIndexService.java
+++ server/src/main/java/com/example/server/service/WalkIndexService.java
@@ -149,8 +149,8 @@ public class WalkIndexService {
         try {
 
             conn = (HttpURLConnection) url.openConnection();
-            conn.setConnectTimeout(5000);
-            conn.setReadTimeout(5000);
+            conn.setConnectTimeout(30000);
+            conn.setReadTimeout(30000);
             conn.setRequestProperty("Content-type", "application/json");
             conn.setDoOutput(true);
 
